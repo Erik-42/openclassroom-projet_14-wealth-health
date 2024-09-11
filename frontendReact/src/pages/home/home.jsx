9c0152c3f1da962b0ba067bf3a3ecf46fde8878d
Erik-42 <mesen.erik@gmail.com>
2024-09-11 18:56:04 +0200

    Maj css bottom

--- frontendReact/src/pages/home/home.jsx
+++ frontendReact/src/pages/home/home.jsx
@@ -7,10 +7,8 @@ import MainLeftBottom from "../../layouts/mainLeftBottom/mainLeftBottom";
 export default function Home() {
 	return (
 		<div className={styled.home}>
-			{/* MainAccueil takes up the top two-thirds */}
 			<MainAccueil className={styled.home__mainAccueil} />
 
-			{/* MainRightBottom and MainLeftBottom share the bottom third */}
 			<div className={styled.home__bottomSection}>
 				<MainLeftBottom className={styled.home__mainLeftBottom} />
 				<MainRightBottom className={styled.home__mainRightBottom} />
