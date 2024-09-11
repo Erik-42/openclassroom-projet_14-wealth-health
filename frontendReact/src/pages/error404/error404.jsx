bfbeb1ac51c2e0027119d69ffad65c141634b634
Erik-42 <mesen.erik@gmail.com>
2024-09-11 18:42:24 +0200

    Ajout page 404

--- frontendReact/src/pages/error404/error404.jsx
+++ frontendReact/src/pages/error404/error404.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styled from "./error404.module.scss";
-import logo404 from "../../assets/img/error404/error404Logo.jpg";
+import logo404 from "../../assets/img/error404/logo404.svg";
 
 export default function Error404() {
 	return (
 		<div className={styled.error404}>
 			<h1>
-				<img src={logo404} alt={logo404} className={styled.error404__logo} />
+				<img src={logo404} alt="Error 404" className={styled.error404__logo} />
 			</h1>
-			<p>Oops! Vous êtes perdu. Cette page n'existe pas!</p>
-			<a href="/" className={styled["home-link"]}>
+			<p className={styled.error404__text}>
+				Oops! Vous êtes perdu. Cette page n'existe pas!
+			</p>
+			<a href="/" className={styled.homeLink}>
 				Retour à l'accueil
 			</a>
 		</div>
