dfd1629fbbe41524717aba810469a3f72e21f2b0
Erik-42 <mesen.erik@gmail.com>
2024-09-24 11:58:52 +0200

    Maj performance

--- composant-npm/modaleReact/src/lib/components/modale/modale.jsx
+++ composant-npm/modaleReact/src/lib/components/modale/modale.jsx
@@ -21,55 +21,11 @@ export default function Modale({
 		<>
 			<ModalDiv data-testid="modal" style={parameter} onClick={closeModale}>
 				<ModalContent>
-					<span style={{ color: "red" }}>{message} test de la modale Erik</span>
+					<span style={{ color: "red" }}>
+						{message} Employé créé avec succés
+					</span>
 					<IMG src={close} alt="close" onClick={closeModale} />
-					<section className={styles.login}>
-						<i className={`fa fa-user-circle ${styles.login__icon}`}></i>
-						<h1>Sign In</h1>
 
-						{errorMessage && (
-							<div className={styles.login__errorMessage}>{errorMessage}</div>
-						)}
-
-						<form className={styles.login__form} onSubmit={handleSubmit}>
-							<div className={styles.login__form__wrapper}>
-								<label htmlFor="email">Email</label>
-								<input
-									id="email"
-									type="email"
-									name="email"
-									required
-									value={login}
-									onChange={(e) => setLogin(e.target.value)}
-								/>
-							</div>
-							<div className={styles.login__form__wrapper}>
-								<label htmlFor="password">Password</label>
-								<input
-									id="password"
-									type="password"
-									name="password"
-									required
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-								/>
-							</div>
-							<div className={styles.login__form__remember}>
-								<input
-									id="rememberMe"
-									type="checkbox"
-									name="persistent"
-									checked={rememberMe}
-									onChange={(e) => setRememberMe(e.target.checked)}
-								/>
-								<label htmlFor="rememberMe">Remember me</label>
-							</div>
-
-							<button type="submit" className={styles.login__button}>
-								Sign In
-							</button>
-						</form>
-					</section>
 					{/* <div className="modale" id={id} style={parameter}>
 						<div className="modale__content">
 							<span
